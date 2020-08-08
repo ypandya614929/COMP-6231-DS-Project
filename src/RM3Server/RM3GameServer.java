414efaf2562824c5e8a9abbfd6329057d382e41d
ypandya614929 <ypandya614929@gmail.com>
2020-08-08 03:06:28 -0400

    Testing completed and some fix

--- src/RM3Server/RM3GameServer.java
+++ src/RM3Server/RM3GameServer.java
@@ -69,15 +69,15 @@ public class RM3GameServer {
 	
 	public RM3GameServer(boolean is_leader) {
 		
-//		try {
-//			writer = new PrintWriter("logs/Testing.txt", "UTF-8");
-//		} catch (FileNotFoundException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		} catch (UnsupportedEncodingException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
+		try {
+			writer = new PrintWriter("logs/Testing.txt", "UTF-8");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		try {
 			
@@ -348,7 +348,7 @@ public class RM3GameServer {
 					response = response.split("#")[0].trim();
 					logger.info("##### " + no);
 					logger.info("Leader Response No : " + no + ", Data : " + response);
-//					writer.println("Leader Response No : " + no + ", Data : " + response);
+					writer.println("Leader Response No : " + no + ", Data : " + response);
 					new DatagramSocket().send(new DatagramPacket(response.getBytes(), response.length(), ia, Constants.FRONTEND_RESPONSE_PORT));
 					response = "";
 					RM1_response = "";
--- src/model/Testing.java
+++ src/model/Testing.java
@@ -35,73 +35,47 @@ public class Testing extends Thread {
 		orb = ORB.init(args, null);
 		org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
 		ncRef = NamingContextExtHelper.narrow(objRef);
-		
-//		PrintStream o = new PrintStream(new File("logs/Testing.txt")); 
-//		PrintStream console = System.out; 
-//		System.setOut(o); 
-		
+
 		Testing t = new Testing();
 		
-		System.out.println("--------------------------- Basic Test Cases ---------------------------\n");
-		System.out.println("Test case detail");
-		System.out.println("----------------");
-		System.out.println("Username : testuserdata1");
-		System.out.println("Password : testuserdata1");
-		System.out.println("Old IP : 182.123.123.123 (Asian)");
-		System.out.println("New IP : 93.123.123.123 (Europe)\n");
-
-		System.out.println("\nTest 1 : Create player account with 182.123.123.123 (Asian) Server");
-		t.create();
-		System.out.println("\nTest 2 : Sign in player account in 182.123.123.123 (Asian) Server");
-		t.signin();
-		System.out.println("\nTest 3 : Get player status");
-		t.getplayerstatus();
-		System.out.println("\nTest 4 : Sign out player account from 182.123.123.123 (Asian) Server");
-		t.signout();
-		System.out.println("\nTest 5 : Transfer player account 182.123.123.123 (Asian) to 93.123.123.123 (Europe) Server");
-		t.transferaccount();
-		System.out.println("\nTest 6 : Try Sign in player account into old Server (182.123.123.123 Asian) after transferting");
-		t.signin();
-		System.out.println("\nTest 7 : Sign in player account into new Server (93.123.123.123 Europe) after transferting");
-		t.signinagain();
-		System.out.println("\nTest 8 : Get player status");
-		t.getplayerstatusagain();
-		System.out.println("\nTest 9 : Suspend player account from 93.123.123.123 (Europe) Server");
-		t.suspendaccount();
-		System.out.println("\nTest 10 : Try Sign in player account after suspended in 93.123.123.123 (Europe) Server");
-		t.signinagain();
-		System.out.println("\nTest 11 : Get player status");
-		t.getplayerstatusagain();
-		System.out.println();
-//		
-//		System.out.println("\n--------------------------- Advanced Test Cases ---------------------------\n");
+//		System.out.println("--------------------------- Basic Test Cases ---------------------------\n");
 //		System.out.println("Test case detail");
 //		System.out.println("----------------");
-//		System.out.println("- I have made 3 threads that run on Asian RM1Server and transfer operations are performed on Europe RM1Server");
-//		System.out.println("\n- Each threads try to create 3 different players with username testuserdata1, testuserdata2 and testuserdata3");
-//		System.out.println("\n- Out of 9 thread calls only 3 will be successful and rest of them contains an error");
-//		System.out.println("\n- Each threads try to suspend testuserdata1 player account only 1 out of 3 will be successful");
-//		System.out.println("\n- Each threads try to transfer testuserdata1 player account if its already suspended than none of thread "
-//				+ "\n  will be successful otherwise only 1 out of 3 will be successful");
-//		System.out.println("\n- Each threads try to transfer testuserdata2 player account only 1 out of 3 will be successful");
-//		System.out.println("\n- Each threads try to suspend testuserdata2 player account if its already transfered than none of thread "
-//				+ "\n  will be successful otherwise only 1 out of 3 will be successful");
-//		System.out.println("\n- I have made 3 threads that run on Asian RM1Server\n");
+//		System.out.println("Username : testuserdata1");
+//		System.out.println("Password : testuserdata1");
+//		System.out.println("Old IP : 182.123.123.123 (Asian)");
+//		System.out.println("New IP : 93.123.123.123 (Europe)\n");
+//
+//		System.out.println("\nTest 1 : Create player account with 182.123.123.123 (Asian) Server");
+//		t.create();
+//		System.out.println("\nTest 2 : Sign in player account in 182.123.123.123 (Asian) Server");
+//		t.signin();
+//		System.out.println("\nTest 3 : Get player status");
+//		t.getplayerstatus();
+//		System.out.println("\nTest 4 : Sign out player account from 182.123.123.123 (Asian) Server");
+//		t.signout();
+//		System.out.println("\nTest 5 : Transfer player account 182.123.123.123 (Asian) to 93.123.123.123 (Europe) Server");
+//		t.transferaccount();
+//		System.out.println("\nTest 6 : Try Sign in player account into old Server (182.123.123.123 Asian) after transferting");
+//		t.signin();
+//		System.out.println("\nTest 7 : Sign in player account into new Server (93.123.123.123 Europe) after transferting");
+//		t.signinagain();
+//		System.out.println("\nTest 8 : Get player status");
+//		t.getplayerstatusagain();
+//		System.out.println("\nTest 9 : Suspend player account from 93.123.123.123 (Europe) Server");
+//		t.suspendaccount();
+//		System.out.println("\nTest 10 : Try Sign in player account after suspended in 93.123.123.123 (Europe) Server");
+//		t.signinagain();
+//		System.out.println("\nTest 11 : Get player status");
+//		t.getplayerstatusagain();
+//		System.out.println();
+//		
+		System.out.println("\n--------------------------- Advanced Test Cases ---------------------------\n");
 		
-		
-//		for(int i=0; i<3; i++) {
-//			new Testing().start();
-//		}
-			
-//		System.out.println("\n- testuserdata1 and testuserdata2 is either suspended or transfered depending on operation result,"
-//				+ "\n however testuserdata3 is always created and offline. result of get player status after all thread execution as below\n");
-//		System.out.println("\nMethod : getPlayerStatus() , " + createAdminObject("182.123.123.123").getPlayerStatus("Admin", "Admin", "182.123.123.123"));
-				
-		// removing players after test execution to avoid from errors
-		// and no need to restart RM1Server again to perform other actions.
-//		createAdminObject("182.123.123.123").suspendAccount("Admin", "Admin", "182.123.123.123", "testuserdata3");
-//		createAdminObject("93.123.123.123").suspendAccount("Admin", "Admin", "93.123.123.123", "testuserdata2");
-				
+		for(int i=0; i<3; i++) {
+			new Testing().start();
+		}
+					
 	}
 	
 	public void create() throws NotFound, CannotProceed, InvalidName, org.omg.CosNaming.NamingContextPackage.InvalidName {
@@ -152,26 +126,33 @@ public class Testing extends Thread {
 	public void run() {
 		
 		try {
+			Thread.sleep((long)(Math.random() * 1000));
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata1");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata1");
 			createAdminObject("182.123.123.123").createPlayerAccount("testdata1", "userdata1", "24", "testuserdata1", "testuserdata1", "182.123.123.123");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata2");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata2");
 			createAdminObject("182.123.123.123").createPlayerAccount("testdata2", "userdata2", "24", "testuserdata2", "testuserdata2", "182.123.123.123");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata3"); 
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : createPlayerAccount() , " + " Username : testuserdata3"); 
 			createAdminObject("182.123.123.123").createPlayerAccount("testdata3", "userdata3", "24", "testuserdata3", "testuserdata3", "182.123.123.123");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : suspendAccount(), Username : testuserdata1");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : suspendAccount(), Username : testuserdata1");
 			createAdminObject("182.123.123.123").suspendAccount("Admin", "Admin", "182.123.123.123", "testuserdata1");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : transferAccount(), Username : testuserdata1");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : transferAccount(), Username : testuserdata1");
 			createAdminObject("182.123.123.123").transferAccount("testuserdata1", "testuserdata1", "182.123.123.123", "93.123.123.123");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : transferAccount(), Username : testuserdata2");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : transferAccount(), Username : testuserdata2");
 			createAdminObject("182.123.123.123").transferAccount("testuserdata2", "testuserdata2", "182.123.123.123", "93.123.123.123");
 			c++;
-			System.out.println("Req Id " + c + " Thread " + Thread.currentThread().getId() + " Method : suspendAccount(), Username : testuserdata2");
+			System.out.println("Req Id " + c + ", Thread " + Thread.currentThread().getId() + " Method : suspendAccount(), Username : testuserdata2");
 			createAdminObject("182.123.123.123").suspendAccount("Admin", "Admin", "182.123.123.123", "testuserdata2");
 			
 		} catch (Exception e) {
